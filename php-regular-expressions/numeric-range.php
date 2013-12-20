6c929eed1863182857bb449e58c50f3d0ef31a9e
Pigo Chu <pigochu@gmail.com>
2013-12-21 00:17:16 +0800

    add 0 or 000..255

--- php-regular-expressions/numeric-range.php
+++ php-regular-expressions/numeric-range.php
@@ -8,6 +8,7 @@ $numerics = array(
 		'255',
 		'256',
 		'0',
+		'01',
 		'-1',
 		'+99',
 		'99',
@@ -35,4 +36,18 @@ echo "Check 0..999\n";
 foreach($numerics as $numeric) {
 	echo "Numeric $numeric is " . (preg_match($pattern, $numeric, $matches) ? 'true' : 'false') . "\n";
 }
+
+$pattern = '/^([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$/';
+echo "Check 0..255\n";
+foreach($numerics as $numeric) {
+	echo "Numeric $numeric is " . (preg_match($pattern, $numeric, $matches) ? 'true' : 'false') . "\n";
+}
+
+$pattern = '/^([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])$/';
+echo "Check 0 or 000..255\n";
+foreach($numerics as $numeric) {
+	echo "Numeric $numeric is " . (preg_match($pattern, $numeric, $matches) ? 'true' : 'false') . "\n";
+}
+
+
 
