2bf687000699196d5042be6128db3670f59774fc
Pigo Chu <pigochu@gmail.com>
2013-12-20 21:45:52 +0800

    Add php reg : numeric range check sample

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ pigo-learn
 ==========
 
 這是我個人學習測試用的
+
+## dart-isolate ##
+
+## php-regular-expressions ##
+以 PHP 寫的通用運算式的範例的收集
+
+- numeric-range.php : 檢查數字範圍
---
+++ php-regular-expressions/numeric-range.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @see http://www.regular-expressions.info/numericranges.html
+ */
+error_reporting(E_ALL);
+$numerics = array(
+		'000',
+		'255',
+		'256',
+		'0',
+		'-1',
+		'+99',
+		'99',
+		'88',
+		'-99',
+		'-555',
+		'123',
+		'1000',
+);
+
+$pattern = '/^([01][0-9][0-9]|2[0-4][0-9]|25[0-5])$/';
+echo "Check 000..255\n";
+foreach($numerics as $numeric) {
+	echo "Numeric $numeric is " . (preg_match($pattern, $numeric, $matches) ? 'true' : 'false') . "\n";	
+}
+
+$pattern = '/^([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])$/';
+echo "Check 0 or 000..255\n";
+foreach($numerics as $numeric) {
+	echo "Numeric $numeric is " . (preg_match($pattern, $numeric, $matches) ? 'true' : 'false') . "\n";
+}
+
+$pattern = '/^([0-9]|[1-9][0-9]|[1-9][0-9][0-9])$/';
+echo "Check 0..999\n";
+foreach($numerics as $numeric) {
+	echo "Numeric $numeric is " . (preg_match($pattern, $numeric, $matches) ? 'true' : 'false') . "\n";
+}
+
